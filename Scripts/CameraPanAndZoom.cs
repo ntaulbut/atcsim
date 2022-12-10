6b2ebfc4f856d32afcf4f3fd3c4f8abcab478ce6
kizza7984 <54994771+kizza7984@users.noreply.github.com>
2022-12-10 23:56:25 +0000

    fix: camera zooms to centre

--- Scripts/CameraPanAndZoom.cs
+++ Scripts/CameraPanAndZoom.cs
@@ -7,28 +7,31 @@ public partial class CameraPanAndZoom : Camera2D
 
     private Vector2 _initialCameraPosition;
     private Vector2 _initialMousePosition;
-    private float _initialZoom;
+
+    private void SetReference()
+    {
+        _initialMousePosition = GetViewport().GetMousePosition();
+        _initialCameraPosition = Position;
+    }
 
     public override void _Input(InputEvent inputEvent)
     {
         if (inputEvent.IsActionPressed("Zoom In"))
         {
             Simulator.Zoom *= (1f + Simulator.ZoomSpeed);
-            //Position *= (1f + Simulator.ZoomSpeed);
-            //_initialCameraPosition *= (1f + Simulator.ZoomSpeed);
+            Position *= (1f + Simulator.ZoomSpeed);
+            SetReference();
         }
         else if (inputEvent.IsActionPressed("Zoom Out"))
         {
             Simulator.Zoom *= (1f - Simulator.ZoomSpeed);
-            //Position *= (1f - Simulator.ZoomSpeed);
-            //_initialCameraPosition *= (1f - Simulator.ZoomSpeed);
+            Position *= (1f - Simulator.ZoomSpeed);
+            SetReference();
         }
         else if (inputEvent.IsActionPressed("Reset Camera"))
         {
             Simulator.Zoom = 1f;
         }
-        Simulator.Zoom = Mathf.Clamp(Simulator.Zoom, 0.1f, 10f);
-        
     }
 
     public override void _Process(double delta)
@@ -36,24 +39,20 @@ public partial class CameraPanAndZoom : Camera2D
         if (Input.IsActionJustPressed("Reset Camera"))
         {
             Position = Vector2.Zero;
-            _initialMousePosition = GetViewport().GetMousePosition();
+            SetReference();
         }
 
         // Set reference points for movement
         if (Input.IsActionJustPressed("Pan Camera"))
         {
-            _initialMousePosition = GetViewport().GetMousePosition();
-            _initialCameraPosition = Position;
-            _initialZoom = Simulator.Zoom;
+            SetReference();
         }
 
         // Move the camera in the opposite direction to mouse movement, from the reference point
         if (Input.IsActionPressed("Pan Camera"))
         {
             Vector2 mouseDelta = GetViewport().GetMousePosition() - _initialMousePosition;
-            Vector2 inverseMouseDelta = new Vector2(-mouseDelta.x, -mouseDelta.y);
-            //Position = (_initialCameraPosition + inverseMouseDelta) * (Simulator.Zoom / _initialZoom);
-            GD.Print(Simulator.Zoom / _initialZoom);
+            Position = _initialCameraPosition + new Vector2(-mouseDelta.x, -mouseDelta.y);
         }
     }
 }
