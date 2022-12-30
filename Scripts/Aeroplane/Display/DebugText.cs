256331481f5810b62ab4de0c07158d33a13af61b
kizza7984 <54994771+kizza7984@users.noreply.github.com>
2022-12-30 21:38:06 +0000

    feat: aeroplane debug text

---
+++ Scripts/Aeroplane/Display/DebugText.cs
@@ -0,0 +1,20 @@
+using Godot;
+
+public partial class DebugText : Label
+{
+    [Export] public Aeroplane Aeroplane;
+
+    public override void _Process(double delta)
+	{
+        float height = Aeroplane.Altitude;
+        Text = string.Format("TAS: {0}kts\nGS: {1}kts\nT: {2}K/{3}C\nSpeed of Sound: {4}m/s\nMach: {5}\nPress: {6}hPa\nPressAlt: {7}ft",
+            Aeroplane.TrueAirspeed,
+            Aeroplane.GroundVector.Length(),
+            AirData.Temperature(height),
+            AirData.Temperature(height) - 273,
+            AirData.SpeedOfSound(height),
+            AirData.Mach(Aeroplane.TrueAirspeed, height),
+            AirData.Pressure(height),
+            AirData.PressureAltitude(height));
+	}
+}
