9176d8f6c0a02b32ad6ce96334b5c1132d3a7dfe
kizza7984 <54994771+kizza7984@users.noreply.github.com>
2023-12-17 01:39:54 +0000

    add 0.1% low frame times to debug text

--- Scripts/Menu/DebugBarText.cs
+++ Scripts/Menu/DebugBarText.cs
@@ -1,13 +1,22 @@
 using Godot;
-using System;
 
 public partial class DebugBarText : Label
 {
+	double worst_1 = 0;
+	double worst_1_age = 0;
+
 	public override void _Process(double delta)
 	{
-		Text = string.Format("{0}fps {1}ms",
-			Engine.GetFramesPerSecond(),
-			Mathf.Round((float)delta * 1000)
-		);
+		double fps = Engine.GetFramesPerSecond();
+		double frame_time = delta * 1000;
+
+		worst_1_age++;
+		if (frame_time > worst_1 || worst_1_age > 1000)
+		{
+			worst_1 = frame_time;
+			worst_1_age = 0;
+		}
+
+		Text = string.Format("{1}fps {2}ms, 0.1% low: {0}ms", Mathf.Round(worst_1), fps, Mathf.Round(frame_time));
 	}
 }
